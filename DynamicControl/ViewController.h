bb63215b559e79e17ddbdde6b84e892888871a8d
Ajay Vishwakarma <ajay.vishwakarma@themobilewallet.com>
2016-06-13 11:24:54 +0530

    Initial Commit

---
+++ DynamicControl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DynamicControl
+//
+//  Created by Ajay on 13/06/16.
+//  Copyright © 2016 Ajay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
